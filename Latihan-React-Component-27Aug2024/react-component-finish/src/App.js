4f1974357faf79db2e86c6dcdd99f6a3fe0ca0d3
Fajar Wahyu Pratama <fajarpratam413@gmail.com>
2024-08-29 17:44:35 +0700

    Add files via upload

---
+++ Latihan-React-Component-27Aug2024/react-component-finish/src/App.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './App.css';
+import Counter from './components/Counter';
+
+const User = (props) => {
+  const { name } = props;
+  return (
+    <div>
+      <h1>My Name is {name}</h1>
+    </div>
+  );
+};
+
+function App() {
+  const [name] = useState('Jhon Doe');
+
+  return (
+    <div className="App">
+      <div>
+        <h1 className="title">React Component</h1>
+        <User name={name} />
+        <Counter initialCount={0} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
